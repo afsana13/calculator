9066c19e37a6d55f7f993d1a63bda037cab5f24d
afsana13 <afsanakhatun13122001@gmail.com>
2023-06-26 19:04:10 +0530

    calculator app

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+print("calculator")
+a=1
+
+b=1234
+print("calculating",('a'+'b'))
+print(a+b)
+''' 
+print("calculating",'a',-,'b')
+print(a-b)
+print("calculating",'a',*,'b')
+print(a*b)'''
---
+++ pracQ1-5.py
@@ -0,0 +1,12 @@
+
+#Q1-poem on twinkle twinkle lil star.
+
+print('''Twinkle, twinkle, little star
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky''')
+
+#Q2- Use REPL and print the table of 5 using it.
+#Q3- Install an external module and use it to perform an operation of your interest.
+
+ 
